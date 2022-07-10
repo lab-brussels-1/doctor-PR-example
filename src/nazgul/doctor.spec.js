fabf50e8c996e5dfd449be428bd2845165bf99e4
NazgulM <92852651+NazgulM@users.noreply.github.com>
2022-07-10 14:45:20 +0200

    test

--- src/nazgul/doctor.spec.js
+++ src/nazgul/doctor.spec.js
@@ -4,28 +4,28 @@
 
 // =============== JSDoc description of the challenge ===============
 /**
-* The government is issuing corona money to healthcareworkers
-* The actual premium has not been decided yet
-* For doctors, the rules are as following:
-*  Less than 10-year experience: x1,5
-*  More than 10 or equal: x2
-*  More than 20 or equal: x3
-*  Never more than: 1000 (throw an error)
-* For nurses:
-*  Less than 10-year experience: x1,2
-*  More than 10 or equal: x1,3
-*  More than 15 or equal: x1,5
-*  More than 20 or equal: x1,7
-*  Never more than: 500 (throw an error)
-* Any other profession should not get a premium (but no error should be thrown)
-*
-* {
+ * The government is issuing corona money to healthcareworkers
+ * The actual premium has not been decided yet
+ * For doctors, the rules are as following:
+ *  Less than 10-year experience: x1,5
+ *  More than 10 or equal: x2
+ *  More than 20 or equal: x3
+ *  Never more than: 1000 (throw an error)
+ * For nurses:
+ *  Less than 10-year experience: x1,2
+ *  More than 10 or equal: x1,3
+ *  More than 15 or equal: x1,5
+ *  More than 20 or equal: x1,7
+ *  Never more than: 500 (throw an error)
+ * Any other profession should not get a premium (but no error should be thrown).
+ *
+ * {
     profession: "nurse" | "doctor" | "therapist" | "psychologist" ,
     experience: 11
-}
-*/
+}.
+ */
 
-/**
+/** ..........
  * Calculate the premium for healthcareworkers
  *
  * @param {object} - The healthcareworker object that is passed to the function
@@ -310,9 +310,7 @@ describe(`${solution.name}: _`, () => {
   describe('premium for nurses', () => {
     describe('When the premium is 500', () => {
       it('Should multiply by x 3 when she has 20 year experience, if premium more then 500 throw error', () => {
-        expect(() =>
-          solution({ profession: 'doctor', experience: 20 }, 500),
-        ).toThrowError();
+        expect(() => solution({ profession: 'doctor', experience: 20 }, 500)).toThrowError();
       });
     });
   });
@@ -322,9 +320,7 @@ describe(`${solution.name}: _`, () => {
   describe('premium for nurses', () => {
     describe('When the premium is 500', () => {
       it('Should multiply by x1.7 when she has less than 10 year experience, if premium more then 500 throw error', () => {
-        expect(() =>
-          solution({ profession: 'doctor', experience: 20 }, 500),
-        ).toThrowError();
+        expect(() => solution({ profession: 'doctor', experience: 20 }, 500)).toThrowError();
       });
     });
   });
@@ -334,9 +330,7 @@ describe(`${solution.name}: _`, () => {
   describe('premium for nurses', () => {
     describe('When the premium is 500', () => {
       it('Should multiply by x1.2 when she has less than 10 year experience, if premium more then 500 throw error', () => {
-        expect(() =>
-          solution({ profession: 'nurse', experience: 8 }, 500),
-        ).toThrowError();
+        expect(() => solution({ profession: 'nurse', experience: 8 }, 500)).toThrowError();
       });
     });
   });
